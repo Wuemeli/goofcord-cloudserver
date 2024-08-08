df2d75baedbfbfb8b8947d974dcd8d340e617c5f
Wuemeli <54443643+Wuemeli@users.noreply.github.com>
2024-08-08 11:21:20 +0200

    Port env option

--- index.ts
+++ index.ts
@@ -106,6 +106,8 @@ app.get('/ping', (req, res) => {
     res.status(200).json({ success: true });
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
